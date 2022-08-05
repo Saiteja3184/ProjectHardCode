c28789e2ba16d5fd24b391155f62b640c2f73ade
Sai Teja <saiteja_saripalli@epam.com>
2022-08-05 16:11:48 +0530

    HardCode

--- src/main/java/com/epamService/Service/PropertyReadPage.java
+++ src/main/java/com/epamService/Service/PropertyReadPage.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class PropertyReadPage {
     public static String readProperty(String key) throws IOException {
-        FileReader fileReader = new FileReader("C:\\Users\\Dr V.SujathaLakshmi\\IdeaProjects\\ProjectHardCode\\src\\test\\resources\\qa.properties");
+        FileReader fileReader = new FileReader("C:\\Users\\Sai Teja\\IdeaProjects\\ProjectHardCode\\src\\test\\resources\\qa.properties");
        // Properties properties;
         Properties properties= new Properties();
         properties.load(fileReader);
